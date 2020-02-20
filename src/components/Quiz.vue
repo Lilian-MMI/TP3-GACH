bdc449ee06f3979b28ed4f650a176a06824929c2
Lilian <ouvrard.l@outlook.com>
2020-02-20 15:03:06 +0100

    Ajout de script dans le fichier quiz.vue, return idQuiz...

---
+++ src/components/Quiz.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="theme">
+    <div>
+        Quiz : {{ idQuiz }}
+  </div>
+</template>
+
+<script>
+import {fsffds} from '../data/adefinir';
+export default {
+    name: "Quiz",
+    props: ["idQuiz]"],
+    data : function(){
+        return{};
+    },
+    computed: {
+        quiz: function(){
+            return getQuizAjax(this.idQuiz);
+        }
+    }
+}
+</script>
+
