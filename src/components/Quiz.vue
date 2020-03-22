101f857596c59015c2d2aeffb6ee0b8d72a126d4
Lucie <luciehello11@gmail.com>
2020-03-22 18:19:44 +0100

    Ajout des questions et réponses dans Quiz.vue

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
+  <div id="example-1">
       <h2>{{ quiz.titre }}</h2>
+      <!--Ajout du nombre de question-->
       Nb Questions :
       {{ listeQuestions ? listeQuestions.length : 0 }}
+      <button v-for="question in listeQuestions" :key="question.id">Question {{ question.id }}</button>
+  <div v-for="question in listeQuestions" :key="question.titre"><br><h3>{{ question.titre }}</h3>
+  <br><input type="radio" id="reponses1" name="Qreponses1">
+  <label for="scales">{{ question['reponses'][0]['titre']}}</label>
+  <br><input type="radio" id="reponses2" name="Qreponses2">
+  <label for="scales">{{ question['reponses'][1]['titre']}}</label>
+  <br><input type="radio" id="reponses3" name="Qreponses3">
+  <label for="scales">{{ question['reponses'][2]['titre']}}</label>
+  </div>
   </div>
 </template>
 <script>
