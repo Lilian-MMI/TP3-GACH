61c174ae92fc54338ac93aadcb469cf2313bab4d
Lucie <luciehello11@gmail.com>
2020-03-22 18:04:29 +0100

    Ajout codes

--- data/jsonQuiz.js
+++
@@ -1,7 +0,0 @@
-async function getQuizAjax(idQuiz) {
-  const resp = await fetch("../data/q${idQuiz}/quiz${idQuiz}");
-  if (resp.ok) {
-    const json = await resp.json();
-    return json;
-  } else throw new Error(resp.status);
-}
