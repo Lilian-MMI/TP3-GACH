1fbae477331b32b54b4ee0662fd8ebb628d77534
Lilian <ouvrard.l@outlook.com>
2020-03-22 18:30:34 +0100

    Ajout de la méthode, et de la fonction executée au clic bouton question

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -2,17 +2,29 @@
   <div id="example-1">
       <h2>{{ quiz.titre }}</h2>
       <!--Ajout du nombre de question-->
-      Nb Questions :
+      Nombre de questions :
       {{ listeQuestions ? listeQuestions.length : 0 }}
-      <button v-for="question in listeQuestions" :key="question.id">Question {{ question.id }}</button>
-  <div v-for="question in listeQuestions" :key="question.titre"><br><h3>{{ question.titre }}</h3>
-  <br><input type="radio" id="reponses1" name="Qreponses1">
-  <label for="scales">{{ question['reponses'][0]['titre']}}</label>
-  <br><input type="radio" id="reponses2" name="Qreponses2">
-  <label for="scales">{{ question['reponses'][1]['titre']}}</label>
-  <br><input type="radio" id="reponses3" name="Qreponses3">
-  <label for="scales">{{ question['reponses'][2]['titre']}}</label>
-  </div>
+      <br>
+      Selection de la question : 
+      <button
+        v-for="question in listeQuestions"
+        :key="question.id"
+        @click="selectQuestion(question.id)"
+      >
+        {{ question.id }}
+      </button>    
+    
+    <div v-for="question in listeQuestions" :key="question.titre"><br><h3>{{ question.titre }}</h3>
+      <br><input type="radio" id="reponses1" name="Qreponses1">
+      <label for="scales">{{ question['reponses'][0]['titre']}}</label>
+      <br><input type="radio" id="reponses2" name="Qreponses2">
+      <label for="scales">{{ question['reponses'][1]['titre']}}</label>
+      <br><input type="radio" id="reponses3" name="Qreponses3">
+      <label for="scales">{{ question['reponses'][2]['titre']}}</label>
+    </div> <!-- div à remplacer par le composant Question --> 
+
+    <Question :idQuestion="idQuestion" @select-question="" /> <!-- A completer ultérieurement --> 
+
   </div>
 </template>
 <script>
@@ -39,6 +51,12 @@ export default {
   computed: {
     quiz : function() {
       return getQuiz(this.idQuiz);
+    }  
+  },
+  methods: {
+    SelectQuestion: function(idQuestion) {
+      this.idQuestion = idQuestion;
+      this.$emit("select-question", idQuestion);
     }
   }
 };
